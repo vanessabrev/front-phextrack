d57438d602d2af8b8f15cba1c8818cfefe6c28d8
Josiel <josielsifa@gmail.com>
2021-01-07 20:18:31 -0300

    Removendo compoenente não utilizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
 import { ContainerComponent } from './components/container/container.component';
-import { InfoHomeComponent } from './services/info-home/info-home.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContainerComponent,
-    InfoHomeComponent,
     PageNotFoundComponent,
     MenuComponent
   ],
