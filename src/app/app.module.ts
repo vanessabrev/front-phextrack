c6ea3fd7817f68a1dbb25b4c29a27b41c08a7640
fariajesiel <fariajesiel@gmail.com>
2021-01-07 22:32:29 -0300

    Trabalhando scroll top sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContainerComponent } from './components/container/container.component';
-import { InfoHomeComponent } from './services/info-home/info-home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { MenuComponent } from './components/menu/menu.component';
 
@@ -14,7 +13,6 @@ import { MenuComponent } from './components/menu/menu.component';
     AppComponent,
     HomeComponent,
     ContainerComponent,
-    InfoHomeComponent,
     PageNotFoundComponent,
     MenuComponent
   ],
