cdb7b8719245f63bae4d3b42a3b64a4236bfbadf
Josiel <josielsifa@gmail.com>
2021-01-11 17:51:00 -0300

    Criando model para enderecos

---
+++ src/app/models/contacts/address.model.ts
@@ -0,0 +1,9 @@
+export class Address {
+  street: string; //rua
+  number: string;
+  complement: string;
+  postalCode: string; //cep
+  city: string;
+  municipality: string; //bairro
+  uf: string;
+}
