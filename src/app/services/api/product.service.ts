fd2022771fbeacbaf51305617e7b451f5657e4a9
Josiel <josielsifa@gmail.com>
2021-01-12 12:06:54 -0300

    Ajustes na estrutura das services

--- src/app/services/api/product.service.ts
+++ src/app/services/api/product.service.ts
@@ -2,12 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Product } from '../models/product.model';
+import { Product } from '../../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductsService {
+export class ProductService {
 
   readonly api = environment.apiUrl;
 
