5f24886437db0d31a788dd95f3dbfeadcc55bc5b
fariajesiel <fariajesiel@gmail.com>
2021-01-12 22:18:42 -0300

    Adicionando dependencias font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { CardProductsComponent } from './components/card-products/card-products.component';
 import { CardComponent } from './components/card/card.component';
+import { CarouselPartnersComponent } from './components/carousel-partners/carousel-partners.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { InfoContactsComponent } from './components/contacts/info-contacts/info-contacts.component';
+import { SendEmailComponent } from './components/contacts/send-email/send-email.component';
 import { ContainerComponent } from './components/container/container.component';
 import { ConvertBase64Component } from './components/convert-base64/convert-base64.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { CarouselPartnersComponent } from './components/carousel-partners/carousel-partners.component';
-import { ContactsComponent } from './components/contacts/contacts.component';
-import { SendEmailComponent } from './components/contacts/send-email/send-email.component';
-import { InfoContactsComponent } from './components/contacts/info-contacts/info-contacts.component';
 import { PostalCodePipe } from './pipes/postal-code.pipe';
 
 
@@ -40,7 +41,8 @@ import { PostalCodePipe } from './pipes/postal-code.pipe';
     BrowserModule,
     AppRoutingModule,
     FlexLayoutModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
