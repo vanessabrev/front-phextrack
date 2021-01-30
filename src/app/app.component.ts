16c802a1594f5a237fadeba5f351510f85eb3d47
Josiel <josielsifa@gmail.com>
2021-01-29 21:32:37 -0300

    AOS - configurando o aos para animacoes na rolagem da pagina

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'phextrack';
+
+  ngOnInit(): void {  AOS.init(); }
 }
