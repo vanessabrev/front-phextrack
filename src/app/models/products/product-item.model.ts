66f29709fd842cf2f954711165ffbf255a6e72a2
Josiel <josielsifa@gmail.com>
2021-01-31 11:24:54 -0300

    Mostrando lista de items para descricao dos produtos

---
+++ src/app/models/products/product-item.model.ts
@@ -0,0 +1,7 @@
+export class ProductItemModel {
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  expiration_date: Date;
+}
--- src/app/models/products/product.model.ts
+++ src/app/models/products/product.model.ts
@@ -1,7 +1,10 @@
+import { ProductItemModel } from "./product-item.model";
+
 export class ProductModel {
   name: string;
   description: string;
   price: number;
   image: string;
   expiration_date: Date;
+  listDescriptions: Array<ProductItemModel>
 }
