25bd08e8b98ad7f9ee1732be2e204d6581565343
Josiel <josielsifa@gmail.com>
2021-01-31 00:27:29 -0300

    Colocando variaveis para auxiliar no desenvolvimento local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const dev = 'http://127.0.0.1:8000';
+const prod = 'http://api.phextrack.com.br';
+
 export const environment = {
   production: false,
-  apiUrl: 'http://api.phextrack.com.br'
+  apiUrl: prod
 };
 
 /*
